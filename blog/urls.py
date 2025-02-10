ff30d887ef589a0a1bf8e63e16cd08fd4b07e2db
navid <169146311+Naviid2024@users.noreply.github.com>
2025-02-10 14:58:53 +0000

    Create comments and posts list view

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('posts/', views.PostList.as_view(), name='post_list'),
+    path('comments/', views.CommentList.as_view(), name='comment_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Post
+from .models import Post, Comment
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().filter(status=1).order_by("-created_on")
     template_name = "post_list.html"
+    context_object_name = "post"
 
 
+class CommentList(generic.ListView):
+    queryset = Comment.objects.all()
+    template_name = "comment_list.html"
+    context_object_name = "comment"
